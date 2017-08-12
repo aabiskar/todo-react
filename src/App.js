037f2cde43320caf77d28c1487a15c4e705e1b46
Manoj <manojkhatri2069@gmail.com>
2017-08-13 00:05:19 +0545

    Completed till 06>Refactor React Components to be stateless Functional Components

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoForm,TodoList} from './components/todo'
+
+class App extends Component {
+
+  
+    constructor() {
+        super();
+        console.log("THIS",this);
+        this.state = {
+            todos:[
+            {id:1,name:'Learn JSX',isComplete:true},
+            {id:2,name:'Build an Awesome App',isComplete:false},
+            {id:3,name:'Ship It',isComplete:false}
+            ],
+            currentTodo:''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        
+    }
+
+    handleInputChange(evt){
+        this.setState({
+            currentTodo :evt.target.value
+        });
+    }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className = "Todo-App">
+       <TodoForm  handleInputChange = {this.handleInputChange}
+          currentTodo = {this.state.currentTodo}/>
+          <TodoList  todos = {this.state.todos}/>
+        
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {TodoItem} from './TodoItem'
+
+export const TodoList = (props) => {
+	return(
+		<div className = "Todo-List">
+            <ul>
+                {props.todos.map(todo =><TodoItem key={todo.id} {...todo}/>)}
+            </ul>
+        </div>)
+}
