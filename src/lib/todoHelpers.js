568cab513f8b53ba086acf63acff6de563d88fa9
Manoj <manojkhatri2069@gmail.com>
2017-08-25 17:51:15 +0545

    09. Update React Application State from Form Input

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -3,5 +3,6 @@
 	
 // }
 
-export const addTodo = (list,item) => [...list,item]
-	
+export const addTodo = (list,item) => [...list,item];
+
+export const generateId = () => Math.floor(Math.random() * 100000);	
